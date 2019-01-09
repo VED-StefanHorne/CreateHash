dc4420348a2c3c8c754be514d40d7787aab144ea
Stefan Horne <46315958+VED-StefanHorne@users.noreply.github.com>
2019-01-09 15:51:11 +0000

    Initial commit

---
+++ CreatePasswordHash/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CreatePasswordHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreatePasswordHash(args[0]));
+        }
+
+        public static string CreatePasswordHash(string password)
+        {
+            string prehashedPassword = GetMd5Hash(password).ToUpper(); // required for compatibility with existing password scheme (uppercase MD5)
+
+            return PasswordHash.PasswordHash.CreateHash(prehashedPassword);
+        }
+
+        public static string GetMd5Hash(string input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                return sBuilder.ToString();
+            }
+        }
+    }
+}
