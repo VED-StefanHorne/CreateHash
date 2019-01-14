ff6c599ae495664935921c8ad06d2683ce5e4403
Stefan Horne <46315958+VED-StefanHorne@users.noreply.github.com>
2019-01-14 14:26:47 +0000

    Change name CreatePasswordHash -> CreateHash

--- CreateHash/Program.cs
+++ CreateHash/Program.cs
@@ -20,10 +20,10 @@ namespace CreatePasswordHash
                 return;
             }
 
-            Console.WriteLine(CreatePasswordHash(args[0]));
+            Console.WriteLine(CreateHash(args[0]));
         }
 
-        public static string CreatePasswordHash(string password)
+        public static string CreateHash(string password)
         {
             string prehashedPassword = GetMd5Hash(password).ToUpper(); // required for compatibility with existing password scheme (uppercase MD5)
 
